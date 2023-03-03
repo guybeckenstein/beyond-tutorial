fabea6b3706ed403d1f6928c3a6347e287d58dc0
Guy Beckenstein <guybec@mta.ac.il>
2023-03-03 13:20:11 +0200

    test CI
    modified setup.sh with another '#'

--- setup.sh
+++ setup.sh
@@ -13,5 +13,6 @@ pipenv sync --dev
 # run our app. setsid, the parentheses and "&" are used to perform a "double
 # fork" so that our app stays up after the setup script finishes.
 # The app logs are redirected to the 'runserver.log' file.
+#
 (setsid pipenv run \
 	python manage.py runserver 0.0.0.0:8000 > runserver.log 2>&1 &)
